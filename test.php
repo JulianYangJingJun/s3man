9fb88740af67c2c9ddaf4c9574e3560073ecc0c7
JulianYangJingJun <aqkt_436@163.com>
2019-07-26 08:11:28 +0800

    feat

--- test.php
+++ test.php
@@ -1,10 +1,8 @@
 <?php
 require_once 'vendor/autoload.php'; // 加载自动加载文件
 
-
-
 use s3man\Extsms;
 $aa = new Extsms();
 $aa->tel = "13858787110";
-$aa->msg = "22222222xxxxx";
+$aa->msg = "222222232342342xxxxx";
 print_r($aa->sendMsg());
