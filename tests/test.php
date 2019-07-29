b03a2fbfceaf5d01bd538013532a8d2186e9ecf1
JulianYangJingJun <aqkt_436@163.com>
2019-07-29 13:20:17 +0800

    feat

--- tests/test.php
+++ tests/test.php
@@ -10,3 +10,16 @@ $aa = new Extsms();
 $aa->tel = "13858787110";
 $aa->msg = "中国国国国国国国国国国国国国男国国国男";
 print_r($aa->sendMsg());
+
+// use services\SendMsg;
+// try {
+//     $aa = new SendMsg();
+//     $aa->tel = 13858787110;
+//     $aa->msg = '中国人中国人asdfasf';
+//     $aa->api_tag = '创建并关联mtm单';
+//     $aa->addOneToSmsLog();
+// } catch (\Exception $e) {
+//     var_dump($e->getMessage());
+// }
+
+// die();
